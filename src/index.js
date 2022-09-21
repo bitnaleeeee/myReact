8fcaa2d9e3ebbad137963036f561cd9865b66718
이빛나 <>
2022-09-21 16:52:11 +0900

    초기화

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import Header from "./components/Header";
+import Header from "./components/Nav/Header";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
