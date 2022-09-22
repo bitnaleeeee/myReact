70904144f0ffa7f8f1448a62ef1ad83853c786f9
recordboy <ksj_design@naver.com>
2022-09-22 22:29:57 +0900

    [테스트] 비주얼 컴포넌트 생성 및 브랜치 테스트

---
+++ src/components/Visual/Visual.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Visual() {
+  return (
+    <div>비주얼 영억</div>
+  );
+}
+
+export default Visual;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from "./components/Nav/Header";
+import Visual from "./components/Visual/Visual";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header />
+  <div>
+    <Header />
+    <Visual />
+  </div>
 );
